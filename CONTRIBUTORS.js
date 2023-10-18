f08b55132c3898218f9750625d40bb6b79426036
Free Software Wing <131646904+fsw-griet@users.noreply.github.com>
2023-10-18 23:23:07 +0530

    Update and rename CONTRIBUTORS.md to CONTRIBUTORS.js
    This is the javascript file where the list of contributors are stored in the json object format

---
+++ CONTRIBUTORS.js
@@ -0,0 +1,22 @@
+// CONTRIBUTORS
+
+// We would like to thank the following contributors for their valuable contributions to this project:
+
+contributors = [
+  {
+    NAME: "Kadali Venkat Amar",
+    YEAR: "3rd",
+    BRANCH: "CSE",
+    COLLEGE: "GRIET",
+    BIO: "Full Stack Web Developer",
+    GITHUB: "https://github.com/kvenkatamar",
+  },
+  
+  // Add more contributors as needed
+  
+];
+
+
+// Thank you for your hard work and dedication to this project!
+
+// Please feel free to open a pull request to add your name to this list if you've made contributions to this project.
